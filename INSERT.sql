74db70a811b0699829075c9dc8677e6457c4d54c
seriogadd <sergejtolokolnikov19@gmail.com>
2024-01-29 15:54:13 +0300

    modified

--- INSERT.sql
+++ INSERT.sql
@@ -1,120 +1,63 @@
+insert into genres (name)
+values 
+('Jazz'),
+('Rock'),
+('Pop'),
+('House');
 
-insert into genres 
-(name)
-values ('Jazz');
-insert into genres 
-(name)
-values ('Rock');
-insert into genres 
-(name)
-values ('Pop');
-insert into genres 
-(name)
-values ('House');
+insert into artists (name) 
+values 
+('Frank Sinatra'),
+('Coldplay'),
+('Lady Gaga'),
+('David Guetta');
 
-insert into artists  
-(name,)
-values ('Frank Sinatra');
+insert into artist_genres (genre_id, artist_id)
+values 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
 
-insert into artists  
-(name)
-values ('Coldplay');
+insert into albums (title, year)
+values
+('The World We Knew', '1967'),
+('Parachutes', '2000'),
+('The Fame', '2008'),
+('Listen', '2014');
 
-insert into artists  
-(name)
-values ('Lady Gaga');
+insert into album_artists (album_id, artist_id)
+values 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
 
-insert into artists  
-(name)
-values ('David Guetta');
+insert into tracks (title, duration, album_id)
+values 
+('Born Free', 204, 1),
+('This Town', 306, 1),
+('Shiver', 501, 2),
+('Spies', 620, 2),
+('Just dance', 406, 3),
+('Love game', 336, 3),
+('Bang my head', 353, 4),
+('Rise (feat. Skylar Grey)', 355, 4);
 
+insert into collections 
+(collection_name, collection_year)
+values
+('The best of AC/DC', 2005);
+('The best of Luios Armstrong', 2011);
+('The best of Shakira', 2015);
+('The best of David Guetta', 2020);
 
-insert into artist_genres
-(genre_id, artist_id)
-values (1, 1);
-
-insert into artist_genres
-(genre_id, artist_id)
-values (2, 2);
-
-insert into artist_genres
-(genre_id, artist_id)
-values (3, 3);
-
-insert into artist_genres
-(genre_id, artist_id)
-values (4, 4);
-
-
-
-
-
-insert into albums
-(title, year)
-values ('The World We Knew', '1967');
-
-insert into albums
-(title, year)
-values ('Parachutes', '2000');
-
-insert into albums
-(title, year)
-values ('The Fame', '2008');
-
-insert into albums
-(title, year)
-values ('Listen', '2014');
-
-
-
-insert into album_artists
-(album_id, artist_id)
-values (1, 1);
-
-insert into album_artists
-(album_id, artist_id)
-values (2, 2);
-
-insert into album_artists
-(album_id, artist_id)
-values (3, 3);
-
-insert into album_artists
-(album_id, artist_id)
-values (4, 4);
-
-
-
-
-
-insert into tracks 
-(title, duration, album_id)
-values ('Born Free', 204, 1);
-
-insert into tracks 
-(title, duration, album_id)
-values ('This Town', 306, 1);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Shiver', 501, 2);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Spies', 620, 2);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Just dance', 406, 3);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Love game', 336, 3);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Bang my head', 353, 4);
-
-insert into tracks 
-(title, duration, album_id)
-values ('Rise (feat. Skylar Grey)', 355, 4);
+insert into tracks_collections
+(track_id, collection_id)
+values 
+(1, 2);
+(2, 2);
+(3, 3);
+(4, 4);
+(5, 5);
+(6, 5);
--- SELECT.sql
+++ SELECT.sql
@@ -1,21 +1,37 @@
--- Количество исполнителей в каждом жанре.
+--Задание 2
+--1. Название и продолжительность самого длительного трека.
+select title, duration from tracks
+order by duration desc
+limit 1
+--2. Название треков, продолжительность которых не менее 3,5 минут.
+select title, round(duration/60, 1) as minutes
+from tracks
+where duration /60 >=350
+--3. Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+select title from collections c 
+where year >= 2018 and year <=2020
+--4. Исполнители, чьё имя состоит из одного слова.
+select name from artists a 
+where name not like '% %'
+--5. Название треков, которые содержат слово «мой» или «my».
+select title  from tracks t 
+where title like '%my%' or title like '%мой%' 
+
+
+
+--Задание 3
+--1. Количество исполнителей в каждом жанре.
 SELECT genres.name, COUNT(artist_genres.artist_id) AS artist_count FROM genres
 LEFT JOIN artist_genres ON genres.id = artist_genres.genre_id
-LEFT JOIN artists ON artist_genres.artist_id = artists.id
-GROUP BY genres.name;
-
--- Количество треков, вошедших в альбомы 2019–2020 годов.
+LEFT JOIN artists ON artist_genres.artist_id = artists.id;
+--2. Количество треков, вошедших в альбомы 2019–2020 годов.
 SELECT COUNT(tracks.id) AS track_count FROM tracks
 LEFT JOIN albums ON tracks.album_id = albums.id
 WHERE albums.year BETWEEN 2019 AND 2020;
-
-
--- Средняя продолжительность треков по каждому альбому.
+--3. Средняя продолжительность треков по каждому альбому.
 SELECT albums.title, AVG(tracks.duration) AS avg_duration FROM albums
-LEFT JOIN tracks ON albums.id = tracks.album_id
-GROUP BY albums.title;
-
--- Все исполнители, которые не выпустили альбомы в 2020 году.
+LEFT JOIN tracks ON albums.id = tracks.album_id;
+--4. Все исполнители, которые не выпустили альбомы в 2020 году.
 SELECT artists.name FROM artists
 WHERE artists.name NOT IN (
     SELECT artists.name FROM artists
@@ -23,19 +39,19 @@ WHERE artists.name NOT IN (
     JOIN albums ON album_artists.album_id = albums.id
     WHERE albums.year = 2020
 )
-GROUP BY artists.name;
-
--- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+--5. Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT collections.title FROM collections
 LEFT JOIN collection_tracks ON collections.id = collection_tracks.collection_id
 LEFT JOIN tracks ON collection_tracks.track_id = tracks.id
 LEFT JOIN albums ON tracks.album_id = albums.id
 LEFT JOIN album_artists ON albums.id = album_artists.album_id
 LEFT JOIN artists ON album_artists.artist_id = artists.id
-WHERE artists.name = 'ИСПОЛНИТЕЛЬ'
-GROUP BY collections.title;
+WHERE artists.name = 'Lady Gaga';
 
--- Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+
+
+--Задание 4 
+--1. Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
 SELECT DISTINCT albums.title
 FROM albums
 JOIN album_artists ON albums.id = album_artists.album_id
@@ -43,13 +59,11 @@ JOIN artists ON album_artists.artist_id = artists.id
 JOIN artist_genres ON artists.id = artist_genres.artist_id
 GROUP BY albums.id, artists.id
 HAVING COUNT(DISTINCT artist_genres.genre_id) > 1;
-
--- Наименования треков, которые не входят в сборники.
+--2. Наименования треков, которые не входят в сборники.
 SELECT tracks.title FROM tracks
 LEFT JOIN collection_tracks ON tracks.id = collection_tracks.track_id
 WHERE collection_tracks.collection_id IS NULL;
-
--- Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
+--3. Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
 SELECT artists.name FROM artists
 LEFT JOIN album_artists ON artists.id = album_artists.artist_id
 LEFT JOIN albums ON album_artists.album_id = albums.id
@@ -57,8 +71,7 @@ LEFT JOIN tracks ON albums.id = tracks.album_id
 WHERE tracks.duration = (
     SELECT MIN(duration) FROM tracks
 );
-
--- Названия альбомов, содержащих наименьшее количество треков.
+--4. Названия альбомов, содержащих наименьшее количество треков.
 SELECT albums.title FROM albums
 JOIN tracks ON albums.id = tracks.album_id
 GROUP BY albums.id
